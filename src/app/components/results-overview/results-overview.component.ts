dd72087a2c5eebe9ef229f198e496be992e5ad16
Samuel Larbi Danquah <larbisamuel0@gmail.com>
2024-08-18 16:23:26 +0000

    Calculates score dynamically

--- src/app/components/results-overview/results-overview.component.ts
+++ src/app/components/results-overview/results-overview.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CategoryService } from '../../services/category.service';
+import { Category } from '../../../ICategory';
 
 @Component({
   selector: 'app-results-overview',
   standalone: true,
   imports: [],
   templateUrl: './results-overview.component.html',
-  styleUrl: './results-overview.component.css'
+  styleUrl: './results-overview.component.css',
 })
 export class ResultsOverviewComponent {
+  score: number = 0;
+  categoryService: CategoryService = inject(CategoryService);
 
+  constructor() {
+    this.categoryService.getAllCategories().then((categories: Category[]) => {
+      console.log(categories);
+      let totalScore = 0;
+      categories.forEach((category) => {
+        totalScore += category.score;
+      });
+      this.score = Math.floor(totalScore / categories.length);
+    });
+  }
 }
